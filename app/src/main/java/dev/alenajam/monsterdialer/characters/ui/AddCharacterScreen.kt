3ea38a3ea2b6c932299133b8b408b31861c17485
Alen Ajam <hello@alenajam.dev>
2026-08-30 17:56:37 +0200

    style: reduce spacing in create/edit character screen

--- app/src/main/java/dev/alenajam/monsterdialer/characters/ui/AddCharacterScreen.kt
+++ app/src/main/java/dev/alenajam/monsterdialer/characters/ui/AddCharacterScreen.kt
@@ -8,6 +8,7 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
@@ -115,7 +116,6 @@ fun AddCharacterScreen(
                     .fillMaxSize()
                     .verticalScroll(rememberScrollState())
                     .padding(24.dp),
-                verticalArrangement = Arrangement.spacedBy(32.dp),
                 horizontalAlignment = Alignment.CenterHorizontally
             ) {
                 // Image Selection Area
@@ -160,6 +160,8 @@ fun AddCharacterScreen(
                         )
                     }
                 }
+
+                Spacer(modifier = Modifier.height(4.dp))
 
                 // Details Card
                 Card(
@@ -211,6 +213,8 @@ fun AddCharacterScreen(
                         )
                     }
                 }
+
+                Spacer(modifier = Modifier.height(4.dp))
 
                 Button(
                     onClick = viewModel::save,
